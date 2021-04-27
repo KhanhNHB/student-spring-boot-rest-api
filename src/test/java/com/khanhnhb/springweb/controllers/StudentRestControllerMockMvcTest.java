dcc88c0dcd91119fd045772f198f1b3b9ba36997
KhanhNHB <hello311016@gmail.com>
2021-04-27 21:37:57 +0700

    Exploring File upload, download, configuration properties spring boot

---
+++ src/test/java/com/khanhnhb/springweb/controllers/StudentRestControllerMockMvcTest.java
@@ -0,0 +1,17 @@
+package com.khanhnhb.springweb.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+public class StudentRestControllerMockMvcTest {
+
+    @Test
+    public void testGetAllStudent() throws Exception {
+
+    }
+
+}
